319d7ffed58c7d0485adc5f138708d9dce385975
mcherouise <mickael.cherouise@ext.soprasteria.com>
2022-02-08 05:50:13 -0800

    Update json.hpp version to 3.10.5
    Signed-off-by: mcherouise <mickael.cherouise@ext.soprasteria.com>

---
+++ include/detail/meta/identity_tag.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace nlohmann
+{
+namespace detail
+{
+// dispatching helper struct
+template <class T>
+struct identity_tag
+{
+};
+}  // namespace detail
+}  // namespace nlohmann
