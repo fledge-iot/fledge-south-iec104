969e834e9302507ca568a9b994420d2b44359f81
ArnauldAlex <arnauld.alex@soprasteria.com>
2022-03-14 22:47:55 +0100

    Add more for all used asdu
    Signed-off-by: ArnauldAlex <arnauld.alex@soprasteria.com>

--- tests/test_asdu_handler.cpp
+++ tests/test_asdu_handler.cpp
@@ -14,64 +14,123 @@ using namespace nlohmann;
 
 struct json_config
 {
-    string protocol_stack =
-        "{\"description\":\"protocolstackparameters\",\"type\":\"string\","
-        "\"displayName\":\"Protocolstackparameters\",\"order\":\"2\","
-        "\"default\":"
-        "{\"protocol_stack\":{\"name\":\"iec104client\",\"version\":\"1.0\","
-        "\"transport_layer\":{\"connection\":{\"path\":[{\"srv_ip\":\"127.0.0."
-        "1\",\"clt_ip\":\"\",\"port\":2404},{\"srv_ip\":\"127.0.0.1\",\"clt_"
-        "ip\":"
-        "\"\",\"port\":2404}],\"tls\":false},\"llevel\":1,\"k_value\":12,\"w_"
-        "value\":8,\"t0_timeout\":10,\"t1_timeout\":15,\"t2_timeout\":10,\"t3_"
-        "timeout\":20,\"conn_all\":true,\"start_all\":false,\"conn_passv\":"
-        "false}"
-        ",\"application_layer\":{\"orig_addr\":0,\"ca_asdu_size\":2,\"ioaddr_"
-        "size\":3,\"startup_time\":180,\"asdu_size\":0,\"gi_time\":60,\"gi_"
-        "cycle\":false,\"gi_all_ca\":false,\"gi_repeat_count\":2,\"disc_qual\":"
-        "\"NT\",\"send_iv_time\":0,\"tsiv\":\"REMOVE\",\"utc_time\":false,"
-        "\"comm_"
-        "wttag\":false,\"comm_parallel\":0,\"exec_cycl_test\":false,\"startup_"
-        "state\":true,\"reverse\":false,\"time_sync\":false}}}}";
+    string protocol_stack = "{\"description\":\"protocolstackparameters\"}";
+
+    string protocol_translation =
+        "{\"description\":\"protocoltranslationmapping\"}";
+
+    string tls = "{\"description\":\"tlsparameters\"}";
 
     string exchanged_data = QUOTE({
         "exchanged_data" : {
             "name" : "iec104client",
             "version" : "1.0",
-            "asdu_list" : [ {
-                "ca" : 41025,
-                "type_id" : "M_ME_NB_1",
-                "label" : "TM-1",
-                "ioa" : 4202832
-            } ]
+            "asdu_list" : [
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_ME_NB_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_SP_NA_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_SP_TB_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_DP_NA_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_DP_TB_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_ST_NA_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_ST_TB_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_ME_NA_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_ME_TD_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_ME_NC_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_ME_TE_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_ME_TF_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "M_EI_NA_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "C_IC_NA_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "C_TS_TA_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "C_SC_TA_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                },
+                {
+                    "ca" : 41025,
+                    "type_id" : "C_DC_TA_1",
+                    "label" : "TM-1",
+                    "ioa" : 4202832
+                }
+            ]
         }
     });
-
-    string protocol_translation =
-        "{\"description\":\"protocoltranslationmapping\",\"type\":"
-        "\"string\","
-        "\"displayName\":\"Protocoltranslationmapping\",\"order\":\"4\","
-        "\"default\":{\"protocol_translation\":{\"name\":\"iec104_to_"
-        "pivot\","
-        "\"version\":\"1.0\",\"mapping\":{\"data_object_header\":{\"doh_"
-        "type\":"
-        "\"type_id\",\"doh_ca\":\"ca\",\"doh_oa\":\"oa\",\"doh_cot\":"
-        "\"cot\","
-        "\"doh_test\":\"istest\",\"doh_negative\":\"isnegative\"},\"data_"
-        "object_"
-        "item\":{\"doi_ioa\":\"ioa\",\"doi_value\":\"value\",\"doi_"
-        "quality\":"
-        "\"quality_desc\",\"doi_ts\":\"time_marker\",\"doi_ts_flag1\":"
-        "\"isinvalid\",\"doi_ts_flag2\":\"isSummerTime\",\"doi_ts_flag3\":"
-        "\"isSubstituted\"}}}}}";
-
-    string tls =
-        "{\"description\":\"tlsparameters\",\"type\":\"string\","
-        "\"displayName\":"
-        "\"TLSparameters\",\"order\":\"5\",\"default\":{\"tls_conf:\":{"
-        "\"private_"
-        "key\":\"server-key.pem\",\"server_cert\":\"server.cer\",\"ca_cert\":"
-        "\"root.cer\"}}}";
 };
 
 class MockIEC104Client : public IEC104Client
@@ -96,50 +155,201 @@ void ingest_cb(void* data, Reading reading) {}
 class AsduHandlerTest : public testing::Test
 {
 protected:
+    CS101_ASDU asdu;
+    CS101_AppLayerParameters alParams;
+    CS104_Slave slave;
+    IEC104* iec104;
+    json_config config;
+    json m_pivot_configuration;
+    InformationObject io;
+    struct sCP56Time2a testTimestamp;
+    MockIEC104Client* m_client;
+
     void SetUp() override
     {
+        m_pivot_configuration = json::parse(config.protocol_translation);
+
         iec104 = new IEC104();
         iec104->registerIngest(NULL, ingest_cb);
+
+        slave = CS104_Slave_create(100, 100);
+        alParams = CS104_Slave_getAppLayerParameters(slave);
+        CP56Time2a_createFromMsTimestamp(&testTimestamp, Hal_getTimeInMs());
+
+        iec104->setJsonConfig(config.protocol_stack, config.exchanged_data,
+                              config.protocol_translation, config.tls);
+
+        m_client = new MockIEC104Client(iec104, &m_pivot_configuration);
+
+        asdu = CS101_ASDU_create(alParams, false, CS101_COT_INITIALIZED, 0, 1,
+                                 false, false);
+        CS101_ASDU_setCA(asdu, 41025);
+
+        // Default IO for destroy purpose
+        io = (InformationObject)MeasuredValueScaled_create(
+            NULL, 4202832, 100, IEC60870_QUALITY_GOOD);
     }
 
-    void TearDown() override {}
-
-    IEC104* iec104;
+    void TearDown() override
+    {
+        CS101_ASDU_destroy(asdu);
+        InformationObject_destroy(io);
+    }
 };
 
-TEST_F(AsduHandlerTest, AsduReceivedHandlerNominal)
+// vector<Datapoint*> datapoints;
+// EXPECT_CALL(m_client,
+//             addData(datapoints, (int64_t)4202832, (std::string) "TM_1",
+//                     testing::Matcher<int64_t>(100),
+//                     IEC60870_QUALITY_GOOD, nullptr));
+
+TEST_F(AsduHandlerTest, AsduReceivedHandlerDefault)
 {
-    json_config config;
+    // Unknown type should return false
+    CS101_ASDU_setTypeID(asdu, F_SC_NB_1);
+    ASSERT_FALSE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    json m_pivot_configuration = json::parse(config.protocol_translation);
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_ME_NB_1)
+{
+    // M_ME_NB_1
+    io = (InformationObject)MeasuredValueScaled_create(NULL, 4202832, 100,
+                                                       IEC60870_QUALITY_GOOD);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    iec104->setJsonConfig(config.protocol_stack, config.exchanged_data,
-                          config.protocol_translation, config.tls);
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_SP_NA_1)
+{
+    // M_SP_NA_1
+    io = (InformationObject)SinglePointInformation_create(
+        NULL, 4202832, true, IEC60870_QUALITY_GOOD);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    MockIEC104Client m_client(iec104, &m_pivot_configuration);
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_SP_TB_1)
+{
+    // M_SP_TB_1
+    io = (InformationObject)SinglePointWithCP56Time2a_create(
+        NULL, 4202832, true, IEC60870_QUALITY_GOOD, &testTimestamp);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    CS104_Slave slave = CS104_Slave_create(100, 100);
-    CS101_AppLayerParameters alParams =
-        CS104_Slave_getAppLayerParameters(slave);
-    CS101_ASDU asdu = CS101_ASDU_create(alParams, false, CS101_COT_INITIALIZED,
-                                        0, 1, false, false);
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_DP_NA_1)
+{
+    // M_DP_NA_1
+    io = (InformationObject)DoublePointInformation_create(
+        NULL, 4202832, IEC60870_DOUBLE_POINT_ON, IEC60870_QUALITY_GOOD);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    CS101_ASDU_setCA(asdu, 41025);
-    CS101_ASDU_setTypeID(asdu, M_ME_NB_1);
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_DP_TB_1)
+{
+    // M_DP_TB_1
+    io = (InformationObject)DoublePointWithCP56Time2a_create(
+        NULL, 4202832, IEC60870_DOUBLE_POINT_ON, IEC60870_QUALITY_GOOD,
+        &testTimestamp);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    InformationObject io = (InformationObject)MeasuredValueScaled_create(
-        NULL, 4202832, 100, IEC60870_QUALITY_GOOD);
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_ST_NA_1)
+{
+    // M_ST_NA_1
+    io = (InformationObject)StepPositionInformation_create(
+        NULL, 4202832, 10, true, IEC60870_QUALITY_GOOD);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    CS101_ASDU_addInformationObject(asdu, io);
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_ST_TB_1)
+{
+    // M_ST_TB_1
+    io = (InformationObject)StepPositionWithCP56Time2a_create(
+        NULL, 4202832, 10, true, IEC60870_QUALITY_GOOD, &testTimestamp);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    // vector<Datapoint*> datapoints;
-    // EXPECT_CALL(m_client,
-    //             addData(datapoints, (int64_t)4202832, (std::string) "TM_1",
-    //                     testing::Matcher<int64_t>(100),
-    //                     IEC60870_QUALITY_GOOD, nullptr));
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_ME_NA_1)
+{
+    // M_ME_NA_1
+    io = (InformationObject)MeasuredValueNormalized_create(
+        NULL, 4202832, 10.0, IEC60870_QUALITY_GOOD);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(&m_client, 0, asdu));
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_ME_TD_1)
+{
+    // M_ME_TD_1
+    io = (InformationObject)MeasuredValueNormalizedWithCP56Time2a_create(
+        NULL, 4202832, 10.0, IEC60870_QUALITY_GOOD, &testTimestamp);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
 
-    InformationObject_destroy(io);
-    CS101_ASDU_destroy(asdu);
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_ME_TE_1)
+{
+    // M_ME_TE_1
+    io = (InformationObject)MeasuredValueScaledWithCP56Time2a_create(
+        NULL, 4202832, 10, IEC60870_QUALITY_GOOD, &testTimestamp);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
+
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_ME_NC_1)
+{
+    // M_ME_NC_1
+    io = (InformationObject)MeasuredValueShort_create(NULL, 4202832, 10.0,
+                                                      IEC60870_QUALITY_GOOD);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
+
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_ME_TF_1)
+{
+    // M_ME_TF_1
+    io = (InformationObject)MeasuredValueShortWithCP56Time2a_create(
+        NULL, 4202832, 10.0, IEC60870_QUALITY_GOOD, &testTimestamp);
+    ASSERT_TRUE(CS101_ASDU_addInformationObject(asdu, io));
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
+
+TEST_F(AsduHandlerTest, AsduReceivedHandlerM_EI_NA_1)
+{
+    // M_EI_NA_1
+    CS101_ASDU_setTypeID(asdu, M_EI_NA_1);
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
+
+TEST_F(AsduHandlerTest, AsduReceivedHandlerC_IC_NA_1)
+{
+    // C_IC_NA_1
+    CS101_ASDU_setTypeID(asdu, C_IC_NA_1);
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
+
+TEST_F(AsduHandlerTest, AsduReceivedHandlerC_TS_TA_1)
+{
+    // C_TS_TA_1
+    CS101_ASDU_setTypeID(asdu, C_TS_TA_1);
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
+
+TEST_F(AsduHandlerTest, AsduReceivedHandlerC_SC_TA_1)
+{
+    // C_SC_TA_1
+    CS101_ASDU_setTypeID(asdu, C_SC_TA_1);
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
+}
+
+TEST_F(AsduHandlerTest, AsduReceivedHandlerC_DC_TA_1)
+{
+    // C_DC_TA_1
+    CS101_ASDU_setTypeID(asdu, C_DC_TA_1);
+    ASSERT_TRUE(iec104->m_asduReceivedHandlerP(m_client, 0, asdu));
 }
