3f9fa9cab02b20bb0a3fc617e40637f29ab06548
Michael Zillgith <michael.zillgith@mz-automation.de>
2023-01-11 07:52:13 +0000

    - added test case
    Signed-off-by: Michael Zillgith <michael.zillgith@mz-automation.de>

--- tests/test_iec104.cpp
+++ tests/test_iec104.cpp
@@ -477,3 +477,73 @@ TEST_F(IEC104Test, IEC104_setJsonConfig_Test)
     ASSERT_NO_THROW(
         iec104->setJsonConfig(protocol_config, exchanged_data, tls_config));
 }
+
+TEST_F(IEC104Test, IEC104_receiveMonitoringAsdusWithCOT_11)
+{
+    ingestCallbackCalled = 0;
+
+    CS104_Slave slave = CS104_Slave_create(10, 10);
+
+    CS104_Slave_setLocalPort(slave, TEST_PORT);
+
+    CS104_Slave_start(slave);
+
+    CS101_AppLayerParameters alParams = CS104_Slave_getAppLayerParameters(slave);
+
+    startIEC104();
+
+    CS101_ASDU newAsdu = CS101_ASDU_create(alParams, false, CS101_COT_RETURN_INFO_REMOTE, 0, 41025, false, false);
+
+    struct sCP56Time2a ts;
+
+    uint64_t timestamp = Hal_getTimeInMs();
+            
+    CP56Time2a_createFromMsTimestamp(&ts, timestamp);
+
+    InformationObject io = (InformationObject) SinglePointWithCP56Time2a_create(NULL, 4206948, true, IEC60870_QUALITY_GOOD, &ts);
+
+    CS101_ASDU_addInformationObject(newAsdu, io);
+
+    InformationObject_destroy(io);
+
+    /* Add ASDU to slave event queue */
+    CS104_Slave_enqueueASDU(slave, newAsdu);
+
+    CS101_ASDU_destroy(newAsdu);
+
+    Thread_sleep(500);
+
+    ASSERT_EQ(ingestCallbackCalled, 1);
+    ASSERT_EQ("TS-1", storedReading->getAssetName());
+    ASSERT_TRUE(hasObject(*storedReading, "data_object"));
+    Datapoint* data_object = getObject(*storedReading, "data_object");
+    ASSERT_NE(nullptr, data_object);
+    ASSERT_TRUE(hasChild(*data_object, "do_type"));
+    ASSERT_TRUE(hasChild(*data_object, "do_ca"));
+    ASSERT_TRUE(hasChild(*data_object, "do_oa"));
+    ASSERT_TRUE(hasChild(*data_object, "do_cot"));
+    ASSERT_TRUE(hasChild(*data_object, "do_test"));
+    ASSERT_TRUE(hasChild(*data_object, "do_negative"));
+    ASSERT_TRUE(hasChild(*data_object, "do_ioa"));
+    ASSERT_TRUE(hasChild(*data_object, "do_value"));
+    ASSERT_TRUE(hasChild(*data_object, "do_quality_iv"));
+    ASSERT_TRUE(hasChild(*data_object, "do_quality_bl"));
+    ASSERT_TRUE(hasChild(*data_object, "do_quality_sb"));
+    ASSERT_TRUE(hasChild(*data_object, "do_quality_nt"));
+    ASSERT_TRUE(hasChild(*data_object, "do_ts"));
+    ASSERT_TRUE(hasChild(*data_object, "do_ts_iv"));
+    ASSERT_TRUE(hasChild(*data_object, "do_ts_su"));
+    ASSERT_TRUE(hasChild(*data_object, "do_ts_sub"));
+
+    ASSERT_EQ("M_SP_TB_1", getStrValue(getChild(*data_object, "do_type")));
+    ASSERT_EQ((int64_t) 41025, getIntValue(getChild(*data_object, "do_ca")));
+    ASSERT_EQ((int64_t) CS101_COT_RETURN_INFO_REMOTE, getIntValue(getChild(*data_object, "do_cot")));
+    ASSERT_EQ((int64_t) 4206948, getIntValue(getChild(*data_object, "do_ioa")));
+    ASSERT_EQ((int64_t) timestamp, getIntValue(getChild(*data_object, "do_ts")));
+
+    delete storedReading;
+
+    CS104_Slave_stop(slave);
+
+    CS104_Slave_destroy(slave);
+}
